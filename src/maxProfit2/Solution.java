f81af31ed5d9f918f585412c8fbae2bd2dd35b9d
ffzs <dz759043338@163.com>
2020-11-08 07:59:16 +0800

    NO.122 买卖股票的最佳时机 II

---
+++ src/maxProfit2/Solution.java
@@ -0,0 +1,24 @@
+package maxProfit2;
+
+/**
+ * @author: ffzs
+ * @Date: 2020/11/8 上午7:16
+ */
+
+public class Solution {
+
+    public int maxProfit(int[] prices) {
+        int i = 0, res = 0;
+        while (i < prices.length) {
+            int start = i;
+            while (i + 1 < prices.length && prices[i] < prices[i+1]) {
+                i++;
+            }
+            if (start != i) {
+                res += (prices[i] - prices[start]);
+            }
+            i++;
+        }
+        return res;
+    }
+}
