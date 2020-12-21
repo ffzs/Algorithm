e7fd0177d0fcc0cc02ff74ed96e5e4fd6c6a63c6
ffzs <dz759043338@163.com>
2020-12-21 09:02:48 +0800

    NO.746 使用最小花费爬楼梯 动态规划

---
+++ src/minCostClimbingStairs/Solution.java
@@ -0,0 +1,21 @@
+package minCostClimbingStairs;
+
+/**
+ * @author: ffzs
+ * @Date: 2020/12/21 上午8:27
+ */
+public class Solution {
+
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+        }
+
+        return Math.min(dp[cost.length-1], dp[cost.length-2]);
+    }
+
+}
