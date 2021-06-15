172559cd1eb733f869b93277673e08c6854599f5
ffzs <dz759043338@163.com>
2021-06-16 07:26:18 +0800

    877. 石子游戏

---
+++ src/stoneGame/Solution.java
@@ -0,0 +1,25 @@
+package stoneGame;
+
+/**
+ * @author: ffzs
+ * @Date: 2021/6/16 上午6:57
+ */
+public class Solution {
+
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
+
+}
