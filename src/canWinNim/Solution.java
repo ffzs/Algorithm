34737149b4071d0d9d5c914732a50e6564956278
ffzs <dz759043338@163.com>
2021-09-18 06:54:56 +0800

    292. Nim 游戏

---
+++ src/canWinNim/Solution.java
@@ -0,0 +1,13 @@
+package canWinNim;
+
+/**
+ * @author: ffzs
+ * @Date: 2021/9/18 上午6:45
+ */
+public class Solution {
+
+    public boolean canWinNim(int n) {
+        return n %4 == 0;
+    }
+
+}
