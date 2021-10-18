0e9ad82ffc74ddfd85d815d6ace43289c0ad72c8
ffzs <dz759043338@163.com>
2021-10-18 17:47:38 +0800

    476. 数字的补数

---
+++ src/findComplement/Solution.java
@@ -0,0 +1,18 @@
+package findComplement;
+
+/**
+ * @author: ffzs
+ * @Date: 2021/10/18 下午5:29
+ */
+public class Solution {
+
+    public int findComplement(int num) {
+        int idx = 1, tmp = num;
+        while ((tmp >>> 1) != 0) {
+            tmp = tmp >>> 1;
+            idx++;
+        }
+        return num ^ ((1 << idx) - 1);
+    }
+
+}
