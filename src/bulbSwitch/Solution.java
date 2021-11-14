00dd8553628e68cb7cdb1f1501cc388222b4eb4b
ffzs <dz759043338@163.com>
2021-11-15 07:34:48 +0800

    319. 灯泡开关

---
+++ src/bulbSwitch/Solution.java
@@ -0,0 +1,13 @@
+package bulbSwitch;
+
+/**
+ * @author: ffzs
+ * @Date: 2021/11/15 上午7:23
+ */
+public class Solution {
+
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n+0.5);
+    }
+
+}
