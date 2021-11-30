37b85d7a965acdd42ad1dc1cac152dc6b3b515e3
ffzs <dz759043338@163.com>
2021-12-01 07:37:32 +0800

    res = Math.max(res, cnt);

---
+++ src/maxPower/Solution.java
@@ -0,0 +1,26 @@
+package maxPower;
+
+/**
+ * @author: ffzs
+ * @Date: 2021/12/1 上午7:33
+ */
+public class Solution {
+
+    public int maxPower(String s) {
+        if (s.length() == 0)
+            return 0;
+        int cnt = 1;
+        int res = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i-1)) cnt++;
+            else {
+                res = Math.max(res, cnt);
+                cnt = 1;
+            }
+        }
+        res = Math.max(res, cnt);
+        return res;
+    }
+
+}
