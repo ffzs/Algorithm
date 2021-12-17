979b24efa760b6d0b36bfbdb76d6de2c98098acd
ffzs <dz759043338@163.com>
2021-12-18 07:55:28 +0800

    419. 甲板上的战舰

---
+++ src/countBattleships/Solution.java
@@ -0,0 +1,22 @@
+package countBattleships;
+
+/**
+ * @author: ffzs
+ * @Date: 2021/12/18 上午7:44
+ */
+public class Solution {
+
+    public int countBattleships(char[][] board) {
+        int m = board.length, n = board[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X') {
+                    if ((j == 0 || board[i][j-1] != 'X') && (i == 0 || board[i-1][j] != 'X')) res++;
+                }
+            }
+        }
+        return res;
+    }
+
+}
