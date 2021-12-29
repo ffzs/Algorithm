9456fb92df84f188323611b2d7ef55d8ef3e1baf
ffzs <dz759043338@163.com>
2021-12-29 08:24:31 +0800

    1995. 统计特殊四元组

--- src/countQuadruplets/Solution.java
+++ src/countQuadruplets/Solution.java
@@ -10,14 +10,17 @@ public class Solution {
         int n = nums.length;
         if (n <= 3) return 0;
         int res = 0;
-        int[] cnt = new int[100*3 + 1];
+        int[] cnt = new int[101];
 
-        for (int i = 2; i < n; ++i) {
-            res += cnt[nums[i]];
-            for (int j = i-1; j > 0; --j) {
-                for (int k = j-1; k >= 0; --k) {
-                    ++cnt[nums[i] + nums[j] + nums[k]];
-                }
+        for (int j = 1; j < n - 2; j++) {
+            for (int i = 0; i < j; i++) {
+                int k = nums[i] + nums[j];
+                if (k < 101) ++cnt[k];
+            }
+
+            for (int l = j + 2; l < n; l++) {
+                int k = nums[l] - nums[j + 1];
+                if (k > 0 && k < 101) res += cnt[k];
             }
         }
         return res;
