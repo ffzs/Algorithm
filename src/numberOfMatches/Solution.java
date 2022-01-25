fc48a6301aa7ba32616a5928595359201264ce6d
ffzs <dz759043338@163.com>
2022-01-25 08:37:24 +0800

    1688. 比赛中的配对次数

---
+++ src/numberOfMatches/Solution.java
@@ -0,0 +1,21 @@
+package numberOfMatches;
+
+/**
+ * @author: ffzs
+ * @Date: 2022/1/25 上午8:29
+ */
+public class Solution {
+    int res = 0;
+    public int numberOfMatches(int n) {
+        if (n == 1) return res;
+        if ((n & 1) == 1) {
+            res += (n - 1) / 2;
+            return numberOfMatches((n - 1) / 2 + 1);
+        }
+        else {
+            res += n / 2;
+            return numberOfMatches(n / 2);
+        }
+    }
+
+}
