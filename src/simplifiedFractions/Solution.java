c437284c7f93012b37695a184ae542d37b600926
ffzs <dz759043338@163.com>
2022-02-10 09:08:10 +0800

    1447. 最简分数

---
+++ src/simplifiedFractions/Solution.java
@@ -0,0 +1,26 @@
+package simplifiedFractions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: ffzs
+ * @Date: 2022/2/10 上午8:57
+ */
+public class Solution {
+
+    public List<String> simplifiedFractions(int n) {
+        boolean[][] ban = new boolean[n+1][n+1];
+        List<String> res = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (!ban[i][j]) res.add(j+"/"+i);
+                for (int k = 2; k * i <= n; k++) {
+                    ban[k*i][k*j] = true;
+                }
+            }
+        }
+        return res;
+    }
+
+}
