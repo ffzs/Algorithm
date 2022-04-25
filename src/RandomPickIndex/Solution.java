daaca79b0842085cb2c9a3d08b36a7a4a9a96528
ffzs <dz759043338@163.com>
2022-04-25 09:03:51 +0800

    398. 随机数索引

---
+++ src/RandomPickIndex/Solution.java
@@ -0,0 +1,29 @@
+package RandomPickIndex;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author: ffzs
+ * @Date: 2022/4/25 上午8:31
+ */
+public class Solution {
+    int[] nums;
+    Random random;
+    public Solution(int[] nums) {
+        this.nums = nums;
+        random = new Random();
+    }
+
+    public int pick(int target) {
+        int res = 0, cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                ++cnt;
+                if (random.nextInt(cnt) == 0) res = i;
+            }
+        }
+        return res;
+    }
+
+}
