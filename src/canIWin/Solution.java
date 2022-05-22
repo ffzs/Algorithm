7a559e2cf4224a62bc3766497134da6be15a86dd
ffzs <dz759043338@163.com>
2022-05-22 08:44:53 +0800

    464. 我能赢吗

---
+++ src/canIWin/Solution.java
@@ -0,0 +1,32 @@
+package canIWin;
+
+/**
+ * @author: ffzs
+ * @Date: 2022/5/22 上午8:18
+ */
+public class Solution {
+
+    int[] memo;
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (maxChoosableInteger >= desiredTotal) return true;
+        if ((1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal) return false;
+        memo = new int[1 << maxChoosableInteger];
+        return dfs(maxChoosableInteger, 0, desiredTotal, 0);
+    }
+
+    private boolean dfs (int max, int num, int desiredTotal, int total) {
+        if (memo[num] == 0) {
+            for (int i = 0; i < max; i++) {
+                if (((num >> i) & 1) == 0) {
+                    int cur = i + 1 + total;
+                    if (cur >= desiredTotal || !dfs(max, num | (1 << i), desiredTotal, cur)) {
+                        memo[num] = 1;
+                        return true;
+                    }
+                }
+            }
+            memo[num] = 2;
+        }
+        return memo[num] == 1;
+    }
+}
