d0bf2e731c23991b763aa68c9f960387fdfb2463
ffzs <dz759043338@163.com>
2022-07-04 10:32:54 +0800

    1200. 最小绝对差

---
+++ src/minimumAbsDifference/Solution.java
@@ -0,0 +1,27 @@
+package minimumAbsDifference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: ffzs
+ * @Date: 2022/7/4 上午10:25
+ */
+public class Solution {
+
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length-1; i++) {
+            diff = Math.min(diff, arr[i+1]-arr[i]);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i+1] - arr[i] == diff) res.add(List.of(arr[i], arr[i+1]));
+        }
+        return res;
+    }
+
+}
