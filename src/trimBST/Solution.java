530cf28a6436ea084850f4149fa58abb9d144cb0
ffzs <dz759043338@163.com>
2022-09-10 12:13:20 +0800

    669. 修剪二叉搜索树

---
+++ src/trimBST/Solution.java
@@ -0,0 +1,36 @@
+package trimBST;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: ffzs
+ * @Date: 2022/9/10 上午9:00
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return null;
+
+        if (root.val < low) return trimBST(root.right, low, high);
+        else if (root.val > high) return trimBST(root.left, low, high);
+        else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+            return root;
+        }
+    }
+}
