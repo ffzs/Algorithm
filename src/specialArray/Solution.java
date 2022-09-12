a30e64955eaff3c8caf83246e9975bcdd0e82c91
ffzs <dz759043338@163.com>
2022-09-12 13:21:29 +0800

    1608. 特殊数组的特征值

--- src/specialArray/Solution.java
+++ src/specialArray/Solution.java
@@ -15,6 +15,7 @@ public class Solution {
         for (int i = n-1, j = 1; i > 0; i-=1, j+=1) {
             if ((nums[i] == j && nums[i] != nums[i-1]) || (nums[i] > j && nums[i-1] < j)) return j;
         }
+
         if (nums[0] >= n) return n;
         return -1;
     }
