5fb2fe3ed112466b849b9273513fe3f247d1f29a
ffzs <dz759043338@163.com>
2022-10-04 15:35:44 +0800

    921. 使括号有效的最少添加

---
+++ src/minAddToMakeValid/Solution.java
@@ -0,0 +1,26 @@
+package minAddToMakeValid;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author: ffzs
+ * @Date: 2022/10/4 下午3:27
+ */
+public class Solution {
+
+    public int minAddToMakeValid(String s) {
+        char[] cs = s.toCharArray();
+        int n = cs.length, res = 0, cnt = 0;
+        for (char c : cs) {
+            if (c == '(') {
+                cnt += 1;
+            } else {
+                if (cnt > 0) cnt -= 1;
+                else res += 1;
+            }
+        }
+        return res;
+    }
+
+}
