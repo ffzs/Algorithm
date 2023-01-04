b0ac1b22750d6ca7ef95556850d3074fc65c7181
ffzs <dz759043338@163.com>
2023-01-04 11:03:18 +0800

    1802. 有界数组中指定下标处的最大值

--- src/maxValue/Solution.java
+++ src/maxValue/Solution.java
@@ -7,17 +7,16 @@ package maxValue;
 public class Solution {
 
     public int maxValue(int n, int index, int maxSum) {
-        int l = index, r = index;
-        int res = 1;
+        long l = 0, r = maxSum;
         maxSum -= n;
-        while (maxSum >= r - l + 1) {
-            res += 1;
-            maxSum -= r - l + 1;
-            if (r < n-1) r++;
-            if (l > 0) l--;
-            if (r == n - 1 && l == 0) return res + maxSum / n;
+        while (l < r) {
+            long mid = (l + r + 1) >>> 1, total = mid * mid;
+            if (mid > index) total -= (mid-index)*(mid-index-1)/2;
+            if (mid > (n - index)) total -= (mid - (n - index - 1)) * (mid - (n - index - 1) - 1) / 2;
+            if (total > maxSum) r = mid - 1;
+            else l = mid;
         }
-        return res;
+        return (int)l + 1;
     }
 
 }
