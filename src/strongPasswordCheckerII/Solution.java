216eae8bea0d10104a70bbebe97ce81d8753c0ff
ffzs <dz759043338@163.com>
2023-01-19 09:23:18 +0800

    2299. 强密码检验器 II

---
+++ src/strongPasswordCheckerII/Solution.java
@@ -0,0 +1,21 @@
+package strongPasswordCheckerII;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/1/19 上午9:12
+ */
+public class Solution {
+    private static String signals = "!@#$%^&*()-+";
+    public boolean strongPasswordCheckerII(String password) {
+        if (password.length() < 8) return false;
+        int lower = 0, upper = 0, digit = 0;
+        char[] cs = password.toCharArray();
+        for (int i = 0; i < cs.length; i++) {
+            if (i != 0 && cs[i] == cs[i-1]) return false;
+            if (cs[i] >= '0' && cs[i] <='9') digit += 1;
+            else if (cs[i] >= 'a' && cs[i] <= 'z') lower += 1;
+            else if (cs[i] >= 'A' && cs[i] <= 'Z') upper += 1;
+        }
+        return lower != 0 && upper != 0 && digit != 0 && (digit + upper + lower) != cs.length;
+    }
+}
