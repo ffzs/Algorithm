80622c765bf73f5b90d8280dbf96db0ccca43fe4
ffzs <dz759043338@163.com>
2023-01-20 15:35:54 +0800

    1817. 查找用户活跃分钟数

---
+++ src/findingUsersActiveMinutes/Solution.java
@@ -0,0 +1,31 @@
+package findingUsersActiveMinutes;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/1/20 下午3:09
+ */
+public class Solution {
+
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        int[] res = new int[k];
+        for(int[] log : logs) {
+            int id = log[0], time = log[1];
+            if (!map.containsKey(id)) map.put(id, new HashSet<>());
+            map.get(id).add(time);
+        }
+        for (Map.Entry<Integer, Set<Integer>> entry: map.entrySet()) {
+            int size = entry.getValue().size();
+            if (size > k) continue;
+            res[size-1] += 1;
+        }
+
+        return res;
+    }
+
+}
