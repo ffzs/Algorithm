f92654110791734efcaf431b4ad65d2d6e237521
ffzs <dz759043338@163.com>
2023-01-23 08:41:06 +0800

    重装个Ubuntu系统对我来说很简单，我为什么2年不重装呢？

--- src/calculateTax/Solution.java
+++ src/calculateTax/Solution.java
@@ -7,19 +7,18 @@ package calculateTax;
 public class Solution {
 
     public double calculateTax(int[][] brackets, int income) {
-        int pre = 0;
-        double res = 0;
+        int pre = 0, res = 0;
         for (int[] bracket : brackets) {
             if (income > bracket[0]) {
-                res += (bracket[0] - pre) * bracket[1] / 100d;
+                res += (bracket[0] - pre) * bracket[1];
             }else {
-                res += (income - pre) * bracket[1] / 100d;
+                res += (income - pre) * bracket[1];
                 break;
             }
             pre = bracket[0];
         }
 
-        return res;
+        return res/100d;
     }
 
 }
