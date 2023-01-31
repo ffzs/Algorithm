cec15ec2fe91a17542b92feddb2b32cc394fbf71
ffzs <dz759043338@163.com>
2023-01-31 16:16:35 +0800

    2319. 判断矩阵是否是一个 X 矩阵

---
+++ src/checkXMatrix/Solution.java
@@ -0,0 +1,24 @@
+package checkXMatrix;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/1/31 下午4:07
+ */
+public class Solution {
+
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    if (grid[i][j] == 0) return false;
+                }
+                else {
+                    if (grid[i][j] != 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
