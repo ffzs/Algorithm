249bccffda3b40b412720867e3ffc54dd5e12532
ffzs <dz759043338@163.com>
2023-02-01 16:05:21 +0800

    2325. 解密消息

---
+++ src/decodeMessage/Solution.java
@@ -0,0 +1,28 @@
+package decodeMessage;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/2/1 下午3:43
+ */
+public class Solution {
+
+    public String decodeMessage(String key, String message) {
+        char[] map = new char[26];
+        char idx = 'a';
+        char[] cs = key.toCharArray();
+        for (char c : cs) {
+            if (c == ' ') continue;
+            if (map[c - 'a'] == 0) {
+                map[c - 'a'] = idx++;
+                if (idx > 'z') break;
+            }
+        }
+        char[] ms = message.toCharArray();
+        for (int i = 0; i < ms.length; i++) {
+            if (ms[i] == ' ') continue;
+            ms[i] = map[ms[i]-'a'];
+        }
+        return new String(ms);
+    }
+
+}
