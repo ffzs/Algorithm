2d96f8f88963bb7b59420f008b11bd66fb43949a
ffzs <dz759043338@163.com>
2023-02-06 11:49:57 +0800

    2331. 计算布尔二叉树的值

---
+++ src/evaluateTree/Solution.java
@@ -0,0 +1,30 @@
+package evaluateTree;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/2/6 上午11:43
+ */
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public boolean evaluateTree(TreeNode root) {
+        if (root.val <= 1) return root.val == 1;
+        if (root.val == 2) return evaluateTree(root.left) || evaluateTree(root.right);
+        else return evaluateTree(root.left) && evaluateTree(root.right);
+    }
+
+}
