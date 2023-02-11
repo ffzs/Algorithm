8014ce22c258d513c6f398154d3b88f68601df33
ffzs <dz759043338@163.com>
2023-02-11 10:00:57 +0800

    2335. 装满杯子需要的最短总时长

---
+++ src/fillCups/Solution.java
@@ -0,0 +1,23 @@
+package fillCups;
+
+import java.util.Arrays;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/2/11 上午9:49
+ */
+public class Solution {
+
+    public int fillCups(int[] amount) {
+        int sum = 0, maxValue = 0;
+        for (int i : amount) {
+            sum += i;
+            maxValue = Math.max(maxValue, i);
+        }
+        System.out.println(sum);
+        if (maxValue >= (sum - maxValue)) return maxValue;
+        // 处理奇偶 + 1
+        return (sum + 1) / 2;
+    }
+
+}
