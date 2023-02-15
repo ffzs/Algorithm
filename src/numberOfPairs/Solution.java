16c2365a4cd0e851beedd1c2ddca93a03f73d3b0
ffzs <dz759043338@163.com>
2023-02-16 06:23:10 +0800

    2341. 数组能形成多少数对

---
+++ src/numberOfPairs/Solution.java
@@ -0,0 +1,23 @@
+package numberOfPairs;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/2/16 上午6:21
+ */
+public class Solution {
+
+    public int[] numberOfPairs(int[] nums) {
+        int[] cnt = new int[101];
+        for (int num : nums) {
+            cnt[num] += 1;
+        }
+
+        int pair = 0, remain = 0;
+        for (int i : cnt) {
+            remain += i % 2;
+            pair += i / 2;
+        }
+        return new int[] {pair, remain};
+    }
+
+}
