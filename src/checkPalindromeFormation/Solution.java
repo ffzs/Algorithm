d2969a71bab2b6f2ae4f24ac7a13b4574ac63603
ffzs <dz759043338@163.com>
2023-03-18 09:07:15 +0800

    1616. 分割两个字符串得到回文串

---
+++ src/checkPalindromeFormation/Solution.java
@@ -0,0 +1,33 @@
+package checkPalindromeFormation;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/3/18 上午8:40
+ */
+public class Solution {
+
+    public boolean checkPalindromeFormation(String a, String b) {
+        char[] ca = a.toCharArray(), cb = b.toCharArray();
+        if (isPalindrome(ca, 0, ca.length-1) || isPalindrome(cb, 0, cb.length - 1)) return true;
+        return check(ca, cb) || check(cb, ca);
+    }
+
+    private boolean isPalindrome (char[] a, int l, int r) {
+        while (l < r) {
+            if (a[l] != a[r]) return false;
+            l += 1;
+            r -= 1;
+        }
+        return true;
+    }
+
+    private boolean check(char[] a, char[] b) {
+        int l = 0, r = a.length - 1;
+        while (l < r && a[l] == b[r]) {
+            l += 1;
+            r -= 1;
+        }
+        return isPalindrome(a, l, r) || isPalindrome(b, l, r);
+    }
+
+}
