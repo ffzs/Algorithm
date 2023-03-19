4b3ceed5199ce0a79f28a44a8a53d6da8ffe328f
ffzs <dz759043338@163.com>
2023-03-19 11:16:46 +0800

    1625. 执行操作后字典序最小的字符串

---
+++ src/findLexSmallestString/Solution.java
@@ -0,0 +1,67 @@
+package findLexSmallestString;
+
+import java.util.Arrays;
+
+/**
+ * @author: ffzs
+ * @Date: 2023/3/19 上午9:43
+ */
+public class Solution {
+
+    public String findLexSmallestString(String s, int a, int b) {
+        boolean[] isValid = new boolean[10];
+        int it = a;
+        while (!isValid[it]) {
+            isValid[it] = true;
+            it += a;
+            it %= 10;
+        }
+
+        int[] trans = new int[10];
+        for (int i = 0; i < 10; i++) {
+            trans[i] = transNum(i, isValid);
+        }
+
+        int n = s.length();
+        String res = s;
+        s = s + s;
+        int g = gcd(b, n);
+
+        for (int i = 0; i < n; i+=g) {
+            char[] cs = s.substring(i, i + n).toCharArray();
+            findIndex(cs, 1, trans[cs[1]-'0']);
+            if ((b & 1) == 1) {
+                findIndex(cs, 0, trans[cs[0]-'0']);
+            }
+            String ss = new String(cs);
+            if (ss.compareTo(res) < 0) res = ss;
+        }
+        return res;
+    }
+
+    private int gcd (int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    private void findIndex (char[] cs, int start, int a) {
+        int diff = cs[start] - '0' - a;
+        for (int i = start; i < cs.length; i+=2) {
+            cs[i] = (char)((cs[i] - '0' - diff + 10) % 10 + '0');
+        }
+//        System.out.println(cs);
+    }
+
+
+    private int transNum (int num, boolean[] isValid) {
+        if (num == 0) return num;
+        for (int i = 0; i <= 9; i++) {
+            if (isValid[i]) {
+                int it = (num + i) % 10;
+                if (it < num) return it;
+            }
+        }
+        return num;
+    }
+
+}
