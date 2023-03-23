fb8389436736e3487840b79bbb6d1e938bb017cf
ffzs <dz759043338@163.com>
2023-03-23 10:13:53 +0800

    1630. 等差子数组

--- src/checkArithmeticSubarrays/Solution.java
+++ src/checkArithmeticSubarrays/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
         List<Boolean> res = new ArrayList<>();
         for (int i = 0; i < l.length; i++) {
-            res.add(checkArithmetic(Arrays.copyOfRange(nums, l[i], r[i]+1)));
+            res.add(checkArithmetic(nums, l[i], r[i]));
         }
 
         return res;
@@ -24,6 +24,27 @@ public class Solution {
         int diff = nums[1] - nums[0];
         for (int i = 2; i < nums.length; i++) {
             if (nums[i] - nums[i-1] != diff) return false;
+        }
+        return true;
+    }
+
+
+    private boolean checkArithmetic (int[] nums, int l, int r) {
+        int maxV = nums[l], minV = nums[l];
+        for (int i = l+1; i <= r; i++) {
+            maxV = Math.max(maxV, nums[i]);
+            minV = Math.min(minV, nums[i]);
+        }
+        if (maxV == minV) return true;
+        int n = r - l;
+        if ((maxV - minV) % n != 0) return false;
+        int d = (maxV - minV) / n;
+        boolean[] t = new boolean[n+1];
+        for (int i = l; i <= r; i++) {
+            if ((nums[i] - minV) % d != 0) return false;
+            int it = (nums[i] - minV)/d;
+            if (t[it]) return false;
+            t[it] = true;
         }
         return true;
     }
