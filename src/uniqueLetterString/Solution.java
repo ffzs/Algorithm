39fc1e3d10f6d54ad41574908ddd9f3b728ae350
ffzs <dz759043338@163.com>
2023-11-26 22:29:48 +0800

    2824. 统计和小于目标的下标对数目

--- src/uniqueLetterString/Solution.java
+++ src/uniqueLetterString/Solution.java
@@ -11,11 +11,12 @@ public class Solution {
     public int uniqueLetterString(String s) {
         char[] cs = s.toCharArray();
         int[] back = new int[26], pre = new int[26];
+        int n = cs.length;
         Arrays.fill(back, -1);
         Arrays.fill(pre, -1);
 
         int res = 0;
-        for (int i = 0; i < cs.length; i++) {
+        for (int i = 0; i < n; i++) {
             int idx = cs[i] - 'A';
             if (pre[idx] != -1) {
                 res += (pre[idx] - back[idx]) * (i - pre[idx]);
@@ -25,7 +26,7 @@ public class Solution {
         }
 
         for (int i = 0; i < 26; i++) {
-            if (pre[i] != -1) res += (pre[i] - back[i]) * (cs.length - pre[i]);
+            if (pre[i] != -1) res += (pre[i] - back[i]) * (n - pre[i]);
         }
         return res;
     }
