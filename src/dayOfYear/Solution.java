eeed9282849978db528465ff6d4f89e02bea64df
ffzs <dz759043338@163.com>
2023-12-31 10:12:00 +0800

    1154. 一年中的第几天

--- src/dayOfYear/Solution.java
+++ src/dayOfYear/Solution.java
@@ -2,11 +2,28 @@ package dayOfYear;
 
 /**
  * @author: ffzs
- * @Date: 2021/12/21 上午7:45
+ * @Date: 2023/12/31
  */
 public class Solution {
     static int[] dayInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
     public int dayOfYear(String date) {
+        int[] day = formatDateString(date);
+        int res = 0;
+        for (int i = 0; i < day[1]-1; i++) {
+            res += dayInMonth[i];
+        }
+
+        res += day[2];
+
+        if ( isRunYear(day) && day[1] > 2) return res + 1;
+        return res;
+    }
+
+    private static boolean isRunYear (int[] day) {
+        return (day[0] % 4 == 0 && day[0] % 100 != 0) || (day[0] % 400 == 0);
+    }
+
+    private static int[] formatDateString(String date){
         int[] day = new int[3];
         int idx = 0, tmp = 0;
         for (char c : date.toCharArray()) {
@@ -20,15 +37,7 @@ public class Solution {
             tmp += c - '0';
         }
         day[2] = tmp;
-        int res = 0;
-        for (int i = 0; i < day[1]-1; i++) {
-            res += dayInMonth[i];
-        }
-
-        res += day[2];
-
-        if (((day[0] % 4 == 0 && day[0] % 100 != 0) || (day[0] % 400 == 0)) && day[1] > 2) res++;
-        return res;
+        return day;
     }
 
 }
